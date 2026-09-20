5f0adbdf4aa209e94b2a48a4c6b0beb7d701709b
chanelrodriguez <chanel.rodriguez336@my.tccd.edu>
2026-09-20 13:46:22 -0500

    Create tailwind-theme.js

---
+++ js/tailwind-theme.js
@@ -0,0 +1,14 @@
+tailwind.config = {
+  theme: {
+    extend: {
+      colors: {
+        cream: "#FBF8F3",
+        medical: { DEFAULT: "#0B6E99", dark: "#084E6E", light: "#E8F4FA" },
+        calm: { DEFAULT: "#0F9B8E", dark: "#0B736A", light: "#E6F7F5" },
+        gold: { DEFAULT: "#C9A227", light: "#F8F1D8" },
+        coral: { DEFAULT: "#E25C4A", dark: "#C44738" }
+      },
+      fontFamily: { vazir: ["Vazirmatn", "sans-serif"] }
+    }
+  }
+};
